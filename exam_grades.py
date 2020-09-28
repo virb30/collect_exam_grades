5e30cbd9c9560d343ff7eeb74ce459d652f7a425
Vinícius Bôscoa <virb30@gmail.com>
2020-09-28 09:34:31 -0300

    add layout constants

--- exam_grades.py
+++ exam_grades.py
@@ -8,17 +8,34 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+INPUT_FILE_LAYOUT = {
+    'cpf': 2,
+    'subscription': 0
+}
+
+EXAM_RESULTS_LAYOUT = {
+    'cpf': 1,
+    'essay': 7,
+    'grades': slice(3,7)
+}
+
+USERNAME = os.environ['WS_USERNAME']
+PASSWORD = os.environ['WS_PASSWORD']
+
 BASE_DIR = './downloads'
 DATE_DIR = date.today().strftime("%Y-%m-%d")
 candidates_dict = {}
 
-def login(driver):
-    username=os.environ['USERNAME']
-    password=os.environ['WS_PASSWORD']
-    driver.find_element_by_id('username').send_keys(username)
-    driver.find_element_by_id('password').send_keys(password)
-    driver.find_element_by_xpath("//input[@type='image']").click()
-    driver.implicitly_wait(5)
+
+def create_directory(directory):
+    if(not os.path.exists(directory)):
+        os.mkdir(directory)
+
+
+def init_directories():
+    create_directory(BASE_DIR)
+    create_directory(f'{BASE_DIR}/{DATE_DIR}')
+    create_directory(f'{BASE_DIR}/{DATE_DIR}/results')
 
 
 def get_result_files():
@@ -33,11 +50,33 @@ def file_exists(search_file):
     return False
 
 
+def get_webdriver():
+    executable_path = r'.\\chromedriver\\chromedriver.exe'
+    chrome_config = {
+        "download.default_directory": os.path.realpath(f'{BASE_DIR}/{DATE_DIR}'),
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "safebrowsing.enabled": True,
+        "profile.default_content_setting_values.automatic_downloads": 2,
+    }
+    options = Options()
+    options.headless = False
+    options.add_experimental_option("prefs", chrome_config)    
+    return webdriver.Chrome(executable_path=executable_path, options=options)
+
+
 def open_browser(driver):
     url = "http://sistemasenem.inep.gov.br/EnemSolicitacao/"
     driver.get(url)
     driver.implicitly_wait(5)
     driver.switch_to.frame(driver.find_element_by_tag_name('iframe'))
+
+
+def login(driver):
+    driver.find_element_by_id('username').send_keys(USERNAME)
+    driver.find_element_by_id('password').send_keys(PASSWORD)
+    driver.find_element_by_xpath("//input[@type='image']").click()
+    driver.implicitly_wait(5)
 
 
 def open_request_form(driver, submenu_id):
@@ -62,21 +101,6 @@ def download_response_file(driver):
     driver.implicitly_wait(2)
 
 
-def get_webdriver():
-    executable_path = r'.\\chromedriver\\chromedriver.exe'
-    chrome_config = {
-        "download.default_directory": os.path.realpath(f'{BASE_DIR}/{DATE_DIR}'),
-        "download.prompt_for_download": False,
-        "download.directory_upgrade": True,
-        "safebrowsing.enabled": True,
-        "profile.default_content_setting_values.automatic_downloads": 2,
-    }
-    options = Options()
-    options.headless = False
-    options.add_experimental_option("prefs", chrome_config)    
-    return webdriver.Chrome(executable_path=executable_path, options=options)
-
-
 def process_file(input_file = 'cpf.txt'):
     driver = get_webdriver()
     open_browser(driver)
@@ -97,33 +121,25 @@ def extract_args():
                         help='input file path')
     parser.add_argument('output_file', metavar='output_file', type=str,
                         help='output file path')
-    parser.add_argument('--col',
-                        help='Defines the column which contains the remote id data to search (default: 0)', default=0, type=int)
     parser.add_argument('--sep',
                         help='Defines separator character of files (default: ;)', default=';')
-    parser.add_argument('--local_col', 
-                        help="Defines the column which contains the local id data to compare (default: 1)", default=1, type=int)
     return parser.parse_args()
 
 
-def generate_output_file(args):
+def generate_request_file():
     global candidates_dict
-
+    args = extract_args()
     input_file = open(args.input_file)
     lines = input_file.readlines()
     with open(args.output_file, mode='w') as output_file:
         for line in lines:
             line_data = line.split(args.sep)
-            write_data = re.sub(r'\D', '', line_data[args.col])
-            candidates_dict[write_data] = line_data[args.local_col]
+            write_data = re.sub(r'\D', '', line_data[INPUT_FILE_LAYOUT['cpf']])
+            candidates_dict[write_data] = line_data[INPUT_FILE_LAYOUT['subscription']]
             output_file.write(f'{write_data}\n')
         output_file.close()
     input_file.close()
-
-
-def create_directory(directory):
-    if(not os.path.exists(directory)):
-        os.mkdir(directory)
+    return args.output_file
 
 
 def calculate_grade_average(grades):
@@ -137,13 +153,14 @@ def calculate_essay_grade(grade):
 
 def save_result_to_file(filepath, result, cpf):
     global candidates_dict
+    print(candidates_dict)
     subscription = candidates_dict.get(cpf)    
     with open(filepath, mode='a') as file:
         file.write(''.join(['X'*40, subscription.zfill(6), 'X'*14, f'{result}\n']))
         file.close()
 
 
-def calculate_grades(args):
+def calculate_grades():
     export_file_date = date.today().strftime('%d%m%Y')     
     grades_output = f'{BASE_DIR}/{DATE_DIR}/results/EXP_{export_file_date}_PON.txt'
     essay_output = f'{BASE_DIR}/{DATE_DIR}/results/EXP_{export_file_date}_RED.txt'
@@ -154,9 +171,9 @@ def calculate_grades(args):
             exam_results = file.readlines()
             for result in exam_results:
                 line_data = result.split(';')
-                cpf = line_data[1]
-                essay_grade = calculate_essay_grade(line_data[7])
-                grades_average = calculate_grade_average(line_data[3:7])
+                cpf = line_data[EXAM_RESULTS_LAYOUT['cpf']]
+                essay_grade = calculate_essay_grade(line_data[EXAM_RESULTS_LAYOUT['essay']])
+                grades_average = calculate_grade_average(line_data[EXAM_RESULTS_LAYOUT['grades']])
                 if(grades_average > 0 and essay_grade > 0):
                     save_result_to_file(grades_output, grades_average, cpf)
                     save_result_to_file(essay_output, essay_grade, cpf)
@@ -165,13 +182,10 @@ def calculate_grades(args):
 
 def main():
     try:
-        args = extract_args()
-        generate_output_file(args)
-        create_directory(BASE_DIR)
-        create_directory(f'{BASE_DIR}/{DATE_DIR}')
-        create_directory(f'{BASE_DIR}/{DATE_DIR}/results')
-        # process_file(args.output_file)
-        calculate_grades(args)
+        init_directories()
+        request_file = generate_request_file()
+        process_file(request_file)
+        calculate_grades()
         print('Script successful executed')
     except Exception as e:
         print(e)
